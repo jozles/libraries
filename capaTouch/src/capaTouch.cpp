00e49bfbde4efee24fd5f33dc6742ed0f1480fd9
jozles <pinkasfeld@combox.fr>
2022-01-27 22:59:36 +0100

    initial

--- capaTouch/src/capaTouch.cpp
+++ capaTouch/src/capaTouch.cpp
@@ -25,11 +25,13 @@ void Capat::init(uint8_t samp,uint8_t common,uint8_t* touch,uint8_t touchNb)
     rReg1 = PIN_TO_BASEREG(touch[0]);
     
     keyNb = touchNb;
-    Serial.print("capaT ");Serial.print(common);Serial.print(" k=");Serial.print(touchNb);Serial.print(" ");delay(1000);
+    Serial.print("capaT ");Serial.print(common);Serial.print(" k=");Serial.print(touchNb);Serial.print(" ");
+    Serial.print(sBit);Serial.print("/");Serial.print((uint32_t)sReg);Serial.print(" ");
+    delay(1000);
     for(uint8_t i=0;i<keyNb;i++){
         rBit[i] = PIN_TO_BITMASK(touch[i]);
         rReg[i] = PIN_TO_BASEREG(touch[i]);
-    Serial.print(touch[i]);if(i<keyNb-1){Serial.print(";");}
+    Serial.print(touch[i]);Serial.print("=");Serial.print(rBit[i]);Serial.print("/");Serial.print((uint32_t)rReg[i]);if(i<keyNb-1){Serial.print(";");}
     }
 //    Serial.println();
 }
@@ -42,7 +44,7 @@ void Capat::calibrate()
 
     uint16_t maxCount[MAXKEY];
     memset(maxCount,0x00,(uint32_t)MAXKEY*sizeof(uint16_t));
-//    Serial.print("s=");Serial.println((uint16_t)sizeof(uint16_t));
+    
     for(uint8_t i=0;i<CALIB;i++){
         count();
         Serial.print(i);
@@ -54,36 +56,11 @@ void Capat::calibrate()
     }
 
     for(uint8_t i=0;i<keyNb;i++){              // precharge to allow mean calculation start
-        //#define MAXMEAN 0x00cf
-        //uint32_t m=(uint32_t)0x01ff*(uint32_t)0x3fff;           //0x007fffff;
-        //totalMem[i]=(uint32_t)MAXMEAN*(uint32_t)NBCNTMEM;
         totalMem[i]=(uint32_t)maxCount[i]*(uint32_t)NBCNTMEM;
-        //meank[i]=MAXMEAN;
         meank[i]=maxCount[i];
         Serial.print(" | ");Serial.print(i);Serial.print(" ");Serial.print(maxCount[i]);Serial.print(" ");Serial.print(lowTresh[i]);Serial.print("-");Serial.print(highTresh[i]);
-        //for(uint8_t j=0;j<NBCNTMEM;j++){
-        //    cntMem[NBCNTMEM*i+j]=MAXMEAN;
-        //}
     }
     Serial.println();
-/*
-    for(uint8_t h=0;h<10;h++){count();}     // heating
-
-    for(uint8_t i=0;i<CALIB;i++){
-        count();delay(100);
-        meanUpdate();
-        Serial.print(i);
-        for(uint8_t j=0;j<keyNb;j++){debounce[j]=0;}
-        
-        for(uint8_t v=0;v<1;v++){
-            Serial.print("(");Serial.print(v);Serial.print(" ");
-            Serial.print(cntk[v]);Serial.print(" ");Serial.print(totalMem[v]);
-            Serial.print(")");
-            //for(uint8_t j=0;j<NBCNTMEM;j++){Serial.print(cntMem[v*NBCNTMEM+j]);Serial.print(",");}
-        }
-        Serial.println();
-    }
-*/    
 }
 
 void Capat::count()
@@ -92,16 +69,11 @@ void Capat::count()
 
     bool rn[MAXKEY];
     bool rt;
-/*
+
     DIRECT_MODE_OUTPUT(sReg, sBit);             // receive output
     DIRECT_WRITE_LOW(sReg, sBit);               // common low    
-*/
-pinMode(13,OUTPUT);
-digitalWrite(13,LOW);
 
     for(uint8_t s=0;s<samples;s++){
-//        Serial.print(s);Serial.print(" ");
-
 /*        for(uint8_t i=0;i<keyNb;i++){
             DIRECT_MODE_INPUT(rReg[i], rBit[i]);    // receive input
             DIRECT_MODE_OUTPUT(rReg[i], rBit[i]);   // receive output
@@ -110,15 +82,10 @@ digitalWrite(13,LOW);
         }
 */
         rt=false;
-/*
+
 DIRECT_MODE_INPUT(rReg[0], rBit[0]);    // receive input
 DIRECT_MODE_OUTPUT(rReg[0], rBit[0]);   // receive output
 DIRECT_WRITE_LOW(rReg[0], rBit[0]);     // receive low
-*/
-pinMode(12,INPUT);
-pinMode(12,OUTPUT);
-digitalWrite(12,LOW);
-
 
         noInterrupts();
 
@@ -127,21 +94,17 @@ digitalWrite(12,LOW);
         }
 */
 
-//DIRECT_MODE_INPUT(rReg[0], rBit[0]);
-pinMode(12,INPUT);
+DIRECT_MODE_INPUT(rReg[0], rBit[0]);
 
-//        DIRECT_WRITE_HIGH(sReg, sBit);              // begin filling common high
-digitalWrite(13,HIGH);
+        DIRECT_WRITE_HIGH(sReg, sBit);              // begin filling common high
+
         while(!rt){                                 // pooling keys until all high
             rt=true;
 /*            for(uint8_t i=0;i<keyNb;i++){
                 if(!rn[i]){cntk[i]++;rn[i]=DIRECT_READ(rReg[i], rBit[i]);rt&=rn[i];}
             }
 */            
-if(
-//    !DIRECT_READ(rReg[0], rBit[0]) 
-    
-    !digitalRead(12) && cntk[0]<MAXCOUNT){cntk[0]++;rt=false;}
+if(!DIRECT_READ(rReg[0], rBit[0]) && cntk[0]<MAXCOUNT){cntk[0]++;rt=false;}
         }
     
         interrupts();
@@ -153,17 +116,11 @@ if(
             rn[i]=true;
         }
 */        
-        rt=true;
-/*
 DIRECT_WRITE_HIGH(rReg[0], rBit[0]);     // receive high    
 DIRECT_MODE_OUTPUT(rReg[0], rBit[0]);    // receive output
 DIRECT_WRITE_HIGH(rReg[0], rBit[0]);     // receive high
-*/
-digitalWrite(12,HIGH);
-pinMode(12,OUTPUT);
-digitalWrite(12,HIGH);
 
-
+        rt=true;
 
         noInterrupts();
 
@@ -171,10 +128,10 @@ digitalWrite(12,HIGH);
             DIRECT_MODE_INPUT(rReg[i], rBit[i]);    // receive input (capacitor is empty)
         }
 */        
-//DIRECT_MODE_INPUT(rReg[0], rBit[0]);    // receive input (capacitor is empty)
-pinMode(12,INPUT);
-//        DIRECT_WRITE_LOW(sReg, sBit);               // common low begin filling    
-digitalWrite(13,LOW);
+DIRECT_MODE_INPUT(rReg[0], rBit[0]);    // receive input (capacitor is empty)
+
+        DIRECT_WRITE_LOW(sReg, sBit);               // common low begin filling    
+
         while(rt){                                  // pooling keys until all low
             rt=false;
 /*            
@@ -182,9 +139,7 @@ digitalWrite(13,LOW);
                 if(rn[i]){cntk[i]++;rn[i]=DIRECT_READ(rReg[i], rBit[i]);rt|=rn[i];}
             }
 */            
-if(
-    //DIRECT_READ(rReg[0], rBit[0])
-    digitalRead(12) && cntk[0]<MAXCOUNT){cntk[0]++;rt=true;}
+if(DIRECT_READ(rReg[0], rBit[0]) && cntk[0]<MAXCOUNT){cntk[0]++;rt=true;}
         }
 /*        for(uint8_t p=0;p<keyNb;p++){Serial.print(cntk[p]);Serial.print(":");}
         Serial.println();
