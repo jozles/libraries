4f80871310149a01b925f85e8c9dbbaf5b86b9e9
jozles <pinkasfeld@combox.fr>
2025-08-27 17:10:10 +0200

    v2.d6 radio
    comments/version

--- Radio/src/radio_const.h
+++ Radio/src/radio_const.h
@@ -1,7 +1,7 @@
 #ifndef _RADIO_CONST_INCLUDED
 #define _RADIO_CONST_INCLUDED
 
-#define VERSION "2.d5"      // 4ème digit pour git
+#define VERSION "2.d6"      // 4ème digit pour git
 #define LENVERSION 4
 
 //#define LORA //NRF
@@ -95,7 +95,7 @@ v2.d  sleepDly corrigé -> le délai est juste à 1% ; la variable globale sleep
          baisse conso sous 13uA @30sec/connexion (changer C5 1uF)
       .4 lpavr_util disparait remplacé par radio_util pour compat concentrateur 
       .5 calibration et <6uA @60s 3.7V -6db
-      .6 ajout gestion paramétrée powerLevel ; correction adcread0
+      .6 ajout gestion paramétrée powerLevel ; correction adcread0 ; calibration pas ok <4.5uA
 
 */
 
